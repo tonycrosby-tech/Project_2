db249412c0e546dac2ae8bd785858f8147886bee
tony913 <tonycrosby96@gmail.com>
2020-12-22 21:08:21 -0600

    error somewhere

--- routes/html/index.js
+++ routes/html/index.js
@@ -37,7 +37,8 @@ module.exports = function (app) {
   // Here we've add our isAuthenticated middleware to this route.
   // If a user who is not logged in tries to access this route they will be redirected to the signup page
   app.get('/members', isAuthenticated, (_req, res) => {
-    res.sendFile(path.join(__dirname, '../../public/members.html'));
+    res.render("members", res);
+    console.log(res);
   });
 };
 
