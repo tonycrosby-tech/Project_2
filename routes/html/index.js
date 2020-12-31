a0a242af8583f6b7c15884a3b59c0fa462574367
Logan Pippin <logan.pippin32@gmail.com>
2020-12-30 22:09:52 -0600

    fixed the lint errors

--- routes/html/index.js
+++ routes/html/index.js
@@ -1,50 +1,46 @@
 // Requiring path to so we can use relative routes to our HTML files
-const path = require("path");
-const db = require("../../models");
+const path = require('path');
+const db = require('../../models');
 // Requiring our custom middleware for checking if a user is logged in
-const isAuthenticated = require("../../config/middleware/isAuthenticated");
+const isAuthenticated = require('../../config/middleware/isAuthenticated');
 // const router = require('express').Router();
-module.exports = function(app) {
-  app.get("/", (req, res) => {
+module.exports = function (app) {
+  app.get('/', (req, res) => {
     // If the user already has an account send them to the members page
     if (req.user) {
-      res.redirect("/members");
+      res.redirect('/members');
     }
-    res.sendFile(path.join(__dirname, "../../public/signup.html"));
+    res.sendFile(path.join(__dirname, '../../public/signup.html'));
   });
   // The GET function.  Come here if the window.replace function tells
   // you to.
-  app.get("/loginAfterSignup", (req, res) => {
-    res.sendFile(path.join(__dirname, "../../public/login.html"));
+  app.get('/loginAfterSignup', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../public/login.html'));
   });
 
-<<<<<<< HEAD
-  app.get("/login", (req, res) => {
-=======
   app.get('/login', (req, res) => {
->>>>>>> ae08f68c6afa6e783881f5d16be3271f950d69a7
     // If the user already has an account send them to the members page
     if (req.user) {
-      res.redirect("/members");
+      res.redirect('/members');
     }
 
-    res.sendFile(path.join(__dirname, "../../public/login.html"));
+    res.sendFile(path.join(__dirname, '../../public/login.html'));
   });
 
   // Route for logging user out
-  app.get("/logout", (req, res) => {
+  app.get('/logout', (req, res) => {
     req.logout();
-    res.redirect("/");
+    res.redirect('/');
   });
   // Here we've add our isAuthenticated middleware to this route.
   // If a user who is not logged in tries to access this route they will be redirected to the signup page
-  app.get("/members", isAuthenticated, (_req, res) => {
+  app.get('/members', isAuthenticated, (_req, res) => {
     // const userEmail = _req.user.email;
 
     // the following code is untested!!!  I don't know that dbPost will automatically
     // reduce itself to posts, categories, users.
     db.Post.findAll({ include: [db.User, db.Comments, db.Category] }).then(
-      function(dbPost) {
+      function (dbPost) {
         // const thebod = dbPost.body.Category;
         // const test2 = dbPost.Category.body;
         // const { Category, User, Comments } = dbPost;
@@ -53,11 +49,6 @@ module.exports = function(app) {
         const commentarray = [];
         const userarray = [];
         const arrayall = [];
-<<<<<<< HEAD
-
-        console.log(dbPost);
-=======
->>>>>>> ae08f68c6afa6e783881f5d16be3271f950d69a7
 
         dbPost.forEach((element) => {
           postarray.push(element.dataValues);
@@ -70,12 +61,7 @@ module.exports = function(app) {
             categories: catarray,
             posts: postarray,
             comments: commentarray,
-<<<<<<< HEAD
-            users: userarray,
-            userEmail: _req.user.email,
-=======
             users: userarray
->>>>>>> ae08f68c6afa6e783881f5d16be3271f950d69a7
           };
 
           arrayall.push(hbsObj);
@@ -88,19 +74,19 @@ module.exports = function(app) {
 
         console.log(hbsObject);
 
-        res.render("members", hbsObject);
+        res.render('members', hbsObject);
       }
     );
 
     // res.render('members', { userEmail });
   });
 
-  app.get("/help", isAuthenticated, (_req, res) => {
-    res.render("help", _req.user);
+  app.get('/help', isAuthenticated, (_req, res) => {
+    res.render('help', _req.user);
   });
 
-  app.get("/forum", isAuthenticated, (_req, res) => {
-    db.Category.findAll({}).then(function(dbCategory) {
+  app.get('/forum', isAuthenticated, (_req, res) => {
+    db.Category.findAll({}).then(function (dbCategory) {
       const catarray = [];
       for (let i = 0; i < dbCategory.length; i++) {
         const cat = dbCategory[i];
@@ -108,49 +94,48 @@ module.exports = function(app) {
         catarray.push(bod);
       }
 
-        const hbsObject = {
-          categories: catarray,
-          userEmail: _req.user.email
-        };
+      const hbsObject = {
+        categories: catarray,
+        userEmail: _req.user.email
+      };
 
-      res.render("forum", hbsObject);
+      res.render('forum', hbsObject);
     });
   });
 
   app.get('/category', isAuthenticated, (_req, res) => {
-    db.Category.findAll({})
-      .then(function (dbCategory) {
-        const catarray = [];
-        for (let i = 0; i < dbCategory.length; i++) {
-          const cat = dbCategory[i];
-          const bod = cat.dataValues;
-          catarray.push(bod);
-        }
+    db.Category.findAll({}).then(function (dbCategory) {
+      const catarray = [];
+      for (let i = 0; i < dbCategory.length; i++) {
+        const cat = dbCategory[i];
+        const bod = cat.dataValues;
+        catarray.push(bod);
+      }
 
-        const hbsObject = {
-          categories: catarray,
-          userEmail: _req.user.email
-        };
+      const hbsObject = {
+        categories: catarray,
+        userEmail: _req.user.email
+      };
 
-        res.render('category', hbsObject);
-      });
+      res.render('category', hbsObject);
+    });
   });
 
   app.get('/about', isAuthenticated, (_req, res) => {
     res.render('about', _req.user);
   });
-  app.get("/home", isAuthenticated, (_req, res) => {
-    res.render("index", _req);
+  app.get('/home', isAuthenticated, (_req, res) => {
+    res.render('index', _req);
   });
 
-  app.get("/posts", isAuthenticated, (req, res) => {
+  app.get('/posts', isAuthenticated, (req, res) => {
     if (req.user) {
       const hsbsObject = {
-        userEmail: req.user.email,
+        userEmail: req.user.email
       };
-      res.render("posts", hsbsObject);
+      res.render('posts', hsbsObject);
     } else {
-      res.sendFile(path.join(__dirname, "../../public/login.html"));
+      res.sendFile(path.join(__dirname, '../../public/login.html'));
     }
   });
 
