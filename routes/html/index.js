136434256fb0706388b939b85a33fc8dfaad90df
Logan Pippin <logan.pippin32@gmail.com>
2021-01-02 13:34:32 -0600

    added a way to look at the individual posts.

--- routes/html/index.js
+++ routes/html/index.js
@@ -1,48 +1,48 @@
 // Requiring path to so we can use relative routes to our HTML files
-const path = require('path');
-const db = require('../../models');
+const path = require("path");
+const db = require("../../models");
 // Requiring our custom middleware for checking if a user is logged in
-const isAuthenticated = require('../../config/middleware/isAuthenticated');
+const isAuthenticated = require("../../config/middleware/isAuthenticated");
 // const router = require('express').Router();
-module.exports = function (app) {
-  app.get('/', (req, res) => {
+module.exports = function(app) {
+  app.get("/", (req, res) => {
     // If the user already has an account send them to the members page
     if (req.user) {
-      res.redirect('/members');
+      res.redirect("/members");
     }
 
-    res.sendFile(path.join(__dirname, '../../public/signup.html'));
+    res.sendFile(path.join(__dirname, "../../public/signup.html"));
   });
   // The GET function.  Come here if the window.replace function tells
   // you to.
-  app.get('/loginAfterSignup', (req, res) => {
-    res.sendFile(path.join(__dirname, '../../public/login.html'));
+  app.get("/loginAfterSignup", (req, res) => {
+    res.sendFile(path.join(__dirname, "../../public/login.html"));
   });
 
-  app.get('/login', (req, res) => {
+  app.get("/login", (req, res) => {
     // If the user already has an account send them to the members page
     if (req.user) {
-      res.redirect('/members');
+      res.redirect("/members");
     }
 
-    res.sendFile(path.join(__dirname, '../../public/login.html'));
+    res.sendFile(path.join(__dirname, "../../public/login.html"));
   });
 
   // Route for logging user out
-  app.get('/logout', (req, res) => {
+  app.get("/logout", (req, res) => {
     req.logout();
-    res.redirect('/');
+    res.redirect("/");
   });
   // Here we've add our isAuthenticated middleware to this route.
   // If a user who is not logged in tries to access this route they will be redirected to the signup page
-  app.get('/members', isAuthenticated, (_req, res) => {
+  app.get("/members", isAuthenticated, (_req, res) => {
     // const userEmail = _req.user.email;
 
     db.Post.findAll({
       include: [db.User, db.Category],
       limit: 10,
-      order: [[db.sequelize.col('updatedAt'), 'DESC']]
-    }).then(function (dbPost) {
+      order: [[db.sequelize.col("updatedAt"), "DESC"]],
+    }).then(function(dbPost) {
       const alltabs = [];
       let hbsObj = {};
 
@@ -53,7 +53,7 @@ module.exports = function (app) {
           bodyCreatedAt: element.dataValues.createdAt,
           name: element.Category.dataValues.name,
           bodyUpdatedAt: element.dataValues.updatedAt,
-          categories: element.dataValues.name
+          categories: element.dataValues.name,
         };
 
         alltabs.push(hbsObj);
@@ -61,19 +61,19 @@ module.exports = function (app) {
 
       const sendObject = {
         postinfo: alltabs,
-        userEmail: _req.user.email
+        userEmail: _req.user.email,
       };
 
-      res.render('members', sendObject);
+      res.render("members", sendObject);
     });
   });
 
-  app.get('/help', isAuthenticated, (_req, res) => {
-    res.render('help', _req.user);
+  app.get("/help", isAuthenticated, (_req, res) => {
+    res.render("help", _req.user);
   });
 
-  app.get('/forum', isAuthenticated, (_req, res) => {
-    db.Category.findAll({}).then(function (dbCategory) {
+  app.get("/forum", isAuthenticated, (_req, res) => {
+    db.Category.findAll({}).then(function(dbCategory) {
       const catarray = [];
       for (let i = 0; i < dbCategory.length; i++) {
         const cat = dbCategory[i];
@@ -83,10 +83,10 @@ module.exports = function (app) {
 
       const hbsObject = {
         categories: catarray,
-        userEmail: _req.user.email
+        userEmail: _req.user.email,
       };
 
-      res.render('forum', hbsObject);
+      res.render("forum", hbsObject);
     });
   });
 
@@ -112,16 +112,16 @@ module.exports = function (app) {
   //   res.render("category", catgoriesGot);
   // });
 
-  app.get('/about', isAuthenticated, (_req, res) => {
-    res.render('about', _req.user);
+  app.get("/about", isAuthenticated, (_req, res) => {
+    res.render("about", _req.user);
   });
-  app.get('/home', isAuthenticated, (_req, res) => {
-    res.render('index', _req);
+  app.get("/home", isAuthenticated, (_req, res) => {
+    res.render("index", _req);
   });
 
-  app.get('/forum/posts', isAuthenticated, (req, res) => {
+  app.get("/forum/posts", isAuthenticated, (req, res) => {
     if (req.user) {
-      db.Category.findAll({}).then(function (dbCategory) {
+      db.Category.findAll({}).then(function(dbCategory) {
         const catarray = [];
         for (let i = 0; i < dbCategory.length; i++) {
           const cat = dbCategory[i];
@@ -131,36 +131,36 @@ module.exports = function (app) {
 
         const hbsObject = {
           categories: catarray,
-          userEmail: req.user.email
+          userEmail: req.user.email,
         };
 
         console.log(hbsObject);
 
-        res.render('posts', hbsObject);
+        res.render("posts", hbsObject);
       });
     } else {
-      res.sendFile(path.join(__dirname, '../../public/login.html'));
+      res.sendFile(path.join(__dirname, "../../public/login.html"));
     }
   });
 
-  app.get('/forum/category/Sports', isAuthenticated, (_req, res) => {
+  app.get("/forum/category/Sports", isAuthenticated, (_req, res) => {
     db.Post.findAll({
       include: [db.User, db.Category],
       limit: 10,
-      order: [[db.sequelize.col('updatedAt'), 'DESC']]
-    }).then(function (dbPost) {
+      order: [[db.sequelize.col("updatedAt"), "DESC"]],
+    }).then(function(dbPost) {
       const alltabs = [];
       let hbsObj = {};
 
       dbPost.forEach((element) => {
-        if (element.Category.dataValues.name === 'Sports') {
+        if (element.Category.dataValues.name === "Sports") {
           hbsObj = {
             title: element.dataValues.title,
             body: element.dataValues.body,
             bodyCreatedAt: element.dataValues.createdAt,
             name: element.Category.dataValues.name,
             bodyUpdatedAt: element.dataValues.updatedAt,
-            userEmail: element.User.dataValues.email
+            userEmail: element.User.dataValues.email,
           };
 
           alltabs.push(hbsObj);
@@ -169,29 +169,29 @@ module.exports = function (app) {
 
       const sendObject = {
         postinfo: alltabs,
-        userEmail: _req.user.email
+        userEmail: _req.user.email,
       };
-      res.render('sports', sendObject);
+      res.render("sports", sendObject);
     });
   });
-  app.get('/forum/category/Movies', isAuthenticated, (_req, res) => {
+  app.get("/forum/category/Movies", isAuthenticated, (_req, res) => {
     db.Post.findAll({
       include: [db.User, db.Category],
       limit: 10,
-      order: [[db.sequelize.col('updatedAt'), 'DESC']]
-    }).then(function (dbPost) {
+      order: [[db.sequelize.col("updatedAt"), "DESC"]],
+    }).then(function(dbPost) {
       const alltabs = [];
       let hbsObj = {};
 
       dbPost.forEach((element) => {
-        if (element.Category.dataValues.name === 'Movies') {
+        if (element.Category.dataValues.name === "Movies") {
           hbsObj = {
             title: element.dataValues.title,
             body: element.dataValues.body,
             bodyCreatedAt: element.dataValues.createdAt,
             name: element.Category.dataValues.name,
             bodyUpdatedAt: element.dataValues.updatedAt,
-            userEmail: element.User.dataValues.email
+            userEmail: element.User.dataValues.email,
           };
 
           alltabs.push(hbsObj);
@@ -200,29 +200,29 @@ module.exports = function (app) {
 
       const sendObject = {
         postinfo: alltabs,
-        userEmail: _req.user.email
+        userEmail: _req.user.email,
       };
-      res.render('movies', sendObject);
+      res.render("movies", sendObject);
     });
   });
-  app.get('/forum/category/Books', isAuthenticated, (_req, res) => {
+  app.get("/forum/category/Books", isAuthenticated, (_req, res) => {
     db.Post.findAll({
       include: [db.User, db.Category],
       limit: 10,
-      order: [[db.sequelize.col('updatedAt'), 'DESC']]
-    }).then(function (dbPost) {
+      order: [[db.sequelize.col("updatedAt"), "DESC"]],
+    }).then(function(dbPost) {
       const alltabs = [];
       let hbsObj = {};
 
       dbPost.forEach((element) => {
-        if (element.Category.dataValues.name === 'Books') {
+        if (element.Category.dataValues.name === "Books") {
           hbsObj = {
             title: element.dataValues.title,
             body: element.dataValues.body,
             bodyCreatedAt: element.dataValues.createdAt,
             name: element.Category.dataValues.name,
             bodyUpdatedAt: element.dataValues.updatedAt,
-            userEmail: element.User.dataValues.email
+            userEmail: element.User.dataValues.email,
           };
 
           alltabs.push(hbsObj);
@@ -231,29 +231,30 @@ module.exports = function (app) {
 
       const sendObject = {
         postinfo: alltabs,
-        userEmail: _req.user.email
+        userEmail: _req.user.email,
       };
-      res.render('books', sendObject);
+      res.render("books", sendObject);
     });
   });
-  app.get('/forum/category/other', isAuthenticated, (_req, res) => {
+  app.get("/forum/category/other", isAuthenticated, (_req, res) => {
     db.Post.findAll({
       include: [db.User, db.Category],
       limit: 10,
-      order: [[db.sequelize.col('updatedAt'), 'DESC']]
-    }).then(function (dbPost) {
+      order: [[db.sequelize.col("updatedAt"), "DESC"]],
+    }).then(function(dbPost) {
       const alltabs = [];
       let hbsObj = {};
 
       dbPost.forEach((element) => {
-        if (element.Category.dataValues.name === 'Other') {
+        if (element.Category.dataValues.name === "Other") {
           hbsObj = {
             title: element.dataValues.title,
             body: element.dataValues.body,
             bodyCreatedAt: element.dataValues.createdAt,
             name: element.Category.dataValues.name,
             bodyUpdatedAt: element.dataValues.updatedAt,
-            userEmail: element.User.dataValues.email
+            userEmail: element.User.dataValues.email,
+            id: element.dataValues.id,
           };
 
           alltabs.push(hbsObj);
@@ -262,10 +263,83 @@ module.exports = function (app) {
 
       const sendObject = {
         postinfo: alltabs,
-        userEmail: _req.user.email
+        userEmail: _req.user.email,
       };
 
-      res.render('other', sendObject);
+      res.render("other", sendObject);
+    });
+  });
+
+  app.get("/forum/category/other/:id", isAuthenticated, (_req, res) => {
+    db.Post.findOne({
+      include: [db.User, db.Category],
+      where: { id: _req.params.id },
+      limit: 10,
+      order: [[db.sequelize.col("updatedAt"), "DESC"]],
+    }).then(function(dbPost) {
+      const hbsObj = {
+        title: dbPost.dataValues.title,
+        body: dbPost.dataValues.body,
+        userEmail: dbPost.User.dataValues.email,
+      };
+
+      console.log(hbsObj);
+
+      res.render("single-post", hbsObj);
+    });
+  });
+  app.get("/forum/category/sports/:id", isAuthenticated, (_req, res) => {
+    db.Post.findOne({
+      include: [db.User, db.Category],
+      where: { id: _req.params.id },
+      limit: 10,
+      order: [[db.sequelize.col("updatedAt"), "DESC"]],
+    }).then(function(dbPost) {
+      const hbsObj = {
+        title: dbPost.dataValues.title,
+        body: dbPost.dataValues.body,
+        userEmail: dbPost.User.dataValues.email,
+      };
+
+      console.log(hbsObj);
+
+      res.render("single-post", hbsObj);
+    });
+  });
+  app.get("/forum/category/movies/:id", isAuthenticated, (_req, res) => {
+    db.Post.findOne({
+      include: [db.User, db.Category],
+      where: { id: _req.params.id },
+      limit: 10,
+      order: [[db.sequelize.col("updatedAt"), "DESC"]],
+    }).then(function(dbPost) {
+      const hbsObj = {
+        title: dbPost.dataValues.title,
+        body: dbPost.dataValues.body,
+        userEmail: dbPost.User.dataValues.email,
+      };
+
+      console.log(hbsObj);
+
+      res.render("single-post", hbsObj);
+    });
+  });
+  app.get("/forum/category/books/:id", isAuthenticated, (_req, res) => {
+    db.Post.findOne({
+      include: [db.User, db.Category],
+      where: { id: _req.params.id },
+      limit: 10,
+      order: [[db.sequelize.col("updatedAt"), "DESC"]],
+    }).then(function(dbPost) {
+      const hbsObj = {
+        title: dbPost.dataValues.title,
+        body: dbPost.dataValues.body,
+        userEmail: dbPost.User.dataValues.email,
+      };
+
+      console.log(hbsObj);
+
+      res.render("single-post", hbsObj);
     });
   });
 };
