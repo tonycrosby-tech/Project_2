476c7169bd35d2ed1ff17cc036b4e6767bfd90b7
tony913 <tonycrosby96@gmail.com>
2021-01-02 10:49:30 -0600

    fixing lint errors

--- server.js
+++ server.js
@@ -1,56 +1,56 @@
 // Requiring necessary npm packages
-require("dotenv").config();
-const express = require("express");
-const exphbs = require("express-handlebars");
-const session = require("express-session");
-const helmet = require("helmet");
-const morgan = require("morgan");
+require('dotenv').config();
+const express = require('express');
+const exphbs = require('express-handlebars');
+const session = require('express-session');
+const helmet = require('helmet');
+const morgan = require('morgan');
 // Requiring passport as we've configured it
-const passport = require("./config/passport");
+const passport = require('./config/passport');
 // const routes = require('./routes');
 
 // Setting up port and requiring models for syncing
 const PORT = process.env.PORT || 8080;
 const SYNC_OPTIONS = {
-  force: true, // process.env.NODE_ENV === 'test'
+  force: true // process.env.NODE_ENV === 'test'
 };
 
-const db = require("./models");
+const db = require('./models');
 
 // Creating express app and configuring middleware needed for authentication
 const app = express();
 
 // Set Handlebars as the default template engine.
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
 
 app.use(
   helmet({
-    contentSecurityPolicy: false,
+    contentSecurityPolicy: false
   })
 );
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static("public"));
+app.use(express.static('public'));
 // We need to use sessions to keep track of our user's login status
 app.use(
   session({
     secret: process.env.SESSION_SECRET,
     resave: true,
-    saveUninitialized: true,
+    saveUninitialized: true
   })
 );
 app.use(passport.initialize());
 app.use(passport.session());
-app.use(morgan("tiny"));
+app.use(morgan('tiny'));
 
 // Requiring our routes
-require("./routes/api/category-api-routes.js")(app);
-require("./routes/api/comments-api-routes.js")(app);
-// require("./routes/api/index.js")(app);
-require("./routes/api/post-api-routes.js")(app);
-require("./routes/api/user-api-routes.js")(app);
-require("./routes/html/index.js")(app);
+require('./routes/api/category-api-routes.js')(app);
+require('./routes/api/comments-api-routes.js')(app);
+// require('./routes/api/index.js')(app);
+require('./routes/api/post-api-routes.js')(app);
+require('./routes/api/user-api-routes.js')(app);
+require('./routes/html/index.js')(app);
 
 // app.use(routes);
 
@@ -58,42 +58,42 @@ require("./routes/html/index.js")(app);
 db.sequelize.sync(SYNC_OPTIONS).then(() => {
   app.listen(PORT, () => {
     db.Category.create({
-      name: "Sports",
-      createdAt: "2020-12-25",
-      updatedAt: "2020-12-25",
+      name: 'Sports',
+      createdAt: '2020-12-25',
+      updatedAt: '2020-12-25'
     })
-      .then(function(dbCategory) {})
+      .then(function (dbCategory) {})
       .catch(() => {});
     db.Category.create({
-      name: "Movies",
-      createdAt: "2020-12-25",
-      updatedAt: "2020-12-25",
+      name: 'Movies',
+      createdAt: '2020-12-25',
+      updatedAt: '2020-12-25'
     })
-      .then(function(dbCategory) {})
+      .then(function (dbCategory) {})
       .catch(() => {
         // do nothing
       });
     db.Category.create({
-      name: "Books",
-      createdAt: "2020-12-25",
-      updatedAt: "2020-12-25",
+      name: 'Books',
+      createdAt: '2020-12-25',
+      updatedAt: '2020-12-25'
     })
-      .then(function(dbCategory) {})
+      .then(function (dbCategory) {})
       .catch(() => {
         // do nothing
       });
     db.Category.create({
-      name: "Other",
-      createdAt: "2020-12-25",
-      updatedAt: "2020-12-25",
+      name: 'Other',
+      createdAt: '2020-12-25',
+      updatedAt: '2020-12-25'
     })
-      .then(function(dbCategory) {})
+      .then(function (dbCategory) {})
       .catch(() => {
         // do nothing
       });
 
     console.log(
-      "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
+      '==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.',
       PORT,
       PORT
     );
