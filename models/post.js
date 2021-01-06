e75e23e2d66c1ac9e9792d11539c1ddc1b7aa012
Logan Pippin <logan.pippin32@gmail.com>
2021-01-05 19:54:03 -0600

    added the character counter.

--- models/post.js
+++ models/post.js
@@ -1,23 +1,23 @@
-module.exports = function (sequelize, DataTypes) {
-  const Post = sequelize.define('Post', {
+module.exports = function(sequelize, DataTypes) {
+  const Post = sequelize.define("Post", {
     title: {
       type: DataTypes.STRING,
       allowNull: true,
-      validate: { len: [1, 15] }
+      validate: { len: [1, 35] },
     },
     body: {
       type: DataTypes.TEXT,
       allowNull: false,
-      validate: { len: [1, 500] }
-    }
+      validate: { len: [1, 500] },
+    },
   });
 
-  Post.associate = function (models) {
+  Post.associate = function(models) {
     Post.belongsTo(models.User, {
-      foreignKey: { allowNull: false }
+      foreignKey: { allowNull: false },
     });
     Post.belongsTo(models.Category, {
-      foreignKey: { allowNull: false }
+      foreignKey: { allowNull: false },
     });
     Post.hasMany(models.Comments, {});
   };
